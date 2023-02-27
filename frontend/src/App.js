eb6c0c18130ac02f1e26c7bc6a1f5d45be34fc4b
Raj Pabari <52271154+rajpabari@users.noreply.github.com>
2023-02-26 21:50:42 -0800

    added props to counter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Counter name="Charlie" age={19} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit Sheden <code>src/App.js</code> and save to reload.
         </p>
-        <Counter />
+
         <a
           className="App-link"
           href="https://reactjs.org"
--- frontend/src/Counter.js
+++ frontend/src/Counter.js
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-function Counter() {
+function Counter(props) {
 
-    const [newCounter, setNewCounter] = useState(0);
+    const [newCounter, setNewCounter] = useState(0)
 
-    useEffect(() => { console.log("newCounter changed") }, [newCounter]);
+    useEffect(() => { console.log("newCounter changed") }, [newCounter])
 
     function handleClick() {
-        setNewCounter(newCounter + 1);
+        setNewCounter(newCounter + 1)
     }
 
     return (
         <>
+            <h1>Welcome to your Dashboard, {props.name} You are {props.age} years old </h1>
             <button onClick={handleClick} >Click me</button>
             <p>Button clicked {newCounter} times</p>
         </>
